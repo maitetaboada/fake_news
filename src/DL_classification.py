668d4c1db6764a9c59e2970ce92c1f2ea672c10a
Fatemah Torabi Asr <ftorabia@d207-023-168-033.wireless.sfu.ca>
2018-01-08 14:49:23 -0800

    DL_classification on liar dataset

--- src/DL_classification.py
+++ src/DL_classification.py
@@ -156,6 +156,39 @@ def prepare_cnn_model_1(word_index, embedding_matrix):
 
 
 
+def prepare_cnn_model_2(word_index, embedding_matrix):
+    embedding_layer = Embedding(len(word_index) + 1,
+                                EMBEDDING_DIM,
+                                weights=[embedding_matrix],
+                                input_length=MAX_SEQUENCE_LENGTH,
+                                trainable=True)
+    convs = []
+    filter_sizes = [3, 4, 5]
+    sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+    embedded_sequences = embedding_layer(sequence_input)
+    for fsz in filter_sizes:
+        l_conv = Conv1D(nb_filter=128, filter_length=fsz, activation='relu')(embedded_sequences)
+        l_pool = MaxPooling1D(5)(l_conv)
+        convs.append(l_pool)
+    l_merge = Merge(mode='concat', concat_axis=1)(convs)
+    l_cov1 = Conv1D(128, 5, activation='relu')(l_merge)
+    l_pool1 = MaxPooling1D(5)(l_cov1)
+    l_cov2 = Conv1D(128, 5, activation='relu')(l_pool1)
+    l_pool2 = MaxPooling1D(30)(l_cov2)
+    l_flat = Flatten()(l_pool2)
+    l_dense = Dense(128, activation='relu')(l_flat)
+    preds = Dense(CLASSES, activation='softmax')(l_dense)
+
+    model = Model(sequence_input, preds)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['acc'])
+    return model
+
+
+
+
+
 
 #texts_train, labels_train = load_data_liar("~/workspace/temp/liar_dataset/train.tsv")
 #texts_test,labels_test = load_data_liar("~/workspace/temp/liar_dataset/test.tsv")
@@ -193,7 +226,7 @@ print('Number of instances from each class')
 print y_train.sum(axis=0)
 print y_val.sum(axis=0)
 
-model = prepare_cnn_model_1(word_index, embedding_matrix)
+model = prepare_cnn_model_2(word_index, embedding_matrix)
 print("model fitting - simplified convolutional neural network")
 model.summary()
 model.fit(x_train, y_train, validation_data=(x_val, y_val),
