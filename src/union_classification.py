b45d3da72ea201fabea810c4b531a5ca4733599f
FA23 <fatemehtorabiasr@yahoo.com>
2018-12-04 11:59:06 -0700

    CV experiments on svc

--- src/union_classification.py
+++ src/union_classification.py
@@ -1,7 +1,10 @@
 
-# Author: Matt Terry <matt.terry@gmail.com>
-#
-# License: BSD 3 clause
+## Used ideas from https://www.kaggle.com/metadist/work-like-a-pro-with-pipelines-and-feature-unions
+## Used ideas from https://www.kaggle.com/edolatabadi/feature-union-with-grid-search
+## Used ideas from https://github.com/scikit-learn/scikit-learn/issues/6122 feature selection output
+
+
+
 from __future__ import print_function
 
 import numpy as np
@@ -18,6 +21,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
+from collections import Counter
 
 
 from textutils import DataLoading
@@ -27,10 +31,79 @@ from nltk.corpus import stopwords
 import nltk
 import pandas as pd
 from sklearn.feature_selection import SelectFromModel
+from sklearn.model_selection import GridSearchCV
+from sklearn.externals import joblib
+
+
+#Reproducibility
+import random
+np.random.seed(0)
+random.seed(0)
 
 
 
+LOAD_DATA_FROM_DISK = True
+CLASSES = 2
 
+#################
+
+print("Preparing lexicons & lwicDic")
+lexicon_directory = "../data/bias_related_lexicons"
+lexicons = []
+lexiconNames = []
+#print("LexiconFeatures() init: loading lexicons")
+for filename in os.listdir(lexicon_directory):
+    if filename.endswith(".txt"):
+        file = os.path.join(lexicon_directory, filename)
+        words = open(file, encoding = "ISO-8859-1").read()
+        lexicon = {k: 0 for k in nltk.word_tokenize(words)}
+        print("Number of terms in the lexicon " + filename + " : " + str(len(lexicon)))
+        lexicons.append(lexicon)
+        lexiconNames.append(filename)
+        continue
+    else:
+        continue
+
+liwcDic = {}
+liwcFile = "../data/lwic/vocabliwc_cats.csv"
+cols = list(pd.read_csv(liwcFile, nrows=1))
+df = pd.read_csv(liwcFile, index_col="Source (A)",
+                 usecols=[i for i in cols if i not in ['WC', 'Analytic', 'Clout', 'Authentic', 'Tone', 'WPS']])
+df = df.T
+keys = df.index
+df = df.reset_index().drop('index', axis='columns')
+cols = df.columns
+values = df.apply(lambda x: x > 0).apply(lambda x: list(cols[x.values]), axis=1)
+liwcDic = dict(zip(keys, values))
+
+#####################
+
+
+class PosTagFeatures(BaseEstimator, TransformerMixin):
+    #normalise = True - devide all values by a total number of tags in the sentence
+    #tokenizer - take a custom tokenizer function
+    def __init__(self, tokenizer=lambda x: x.split(), normalize=True):
+        print("Inside the init function of PosTagFeatures")
+        self.tokenizer=tokenizer
+        self.normalize=normalize
+
+    #helper function to tokenize and count parts of speech
+    def pos_func(self, sentence):
+        return Counter(tag for word,tag in nltk.pos_tag(self.tokenizer(sentence), tagset='universal'))
+
+    # fit() doesn't do anything, this is a transformer class
+    def fit(self, X, y = None):
+        return self
+
+    #all the work is done here
+    def transform(self, X):
+        X = pd.Series(X)
+        X_tagged = X.apply(self.pos_func).apply(pd.Series).fillna(0)
+        X_tagged['n_tokens'] = X_tagged.apply(sum, axis=1)
+        if self.normalize:
+            X_tagged = X_tagged.divide(X_tagged['n_tokens'], axis=0)
+
+        return X_tagged
 
 
 
@@ -56,9 +129,9 @@ class SurfaceFeatures(BaseEstimator, TransformerMixin):
         print("In SurfaceFeatures: avg length and avg num_sentences ")
         avgLength = sum(item['length'] for item in features) / len(features)
         avgSentences = sum(item['num_sentences'] for item in features) / len(features)
-        print(avgLength)
-        print(avgSentences)
-        print(features[0:10])
+        #print(avgLength)
+        #print(avgSentences)
+        #print(features[0:10])
 
         return features
 
@@ -67,6 +140,7 @@ class LiwcFeatures(BaseEstimator, TransformerMixin):
     liwcDic = {} #= map of text to dataframe row (this dataframe should be read from the file including liwc features)
 
     def __init__(self):
+        '''
         liwcFile = "../data/lwic/vocabliwc_cats.csv"
         cols = list(pd.read_csv(liwcFile, nrows=1))
         df = pd.read_csv(liwcFile, index_col="Source (A)",
@@ -79,10 +153,10 @@ class LiwcFeatures(BaseEstimator, TransformerMixin):
         cols = df.columns
         values = df.apply(lambda x: x > 0).apply(lambda x: list(cols[x.values]), axis=1)
         self.liwcDic = dict(zip(keys, values))
-        print("Testing the dictionary made for liwc feature 'function':")
-        print(self.liwcDic['function'])
-
-
+        #print("Testing the dictionary made for liwc feature 'function':")
+        #print(self.liwcDic['function'])
+        '''
+        self.liwcDic = liwcDic
 
     def fit(self, x, y=None):
         return self
@@ -117,10 +191,11 @@ class LexiconFeatures(BaseEstimator, TransformerMixin):
 
     def __init__(self):
         print("Inside the init function of LexiconFeatures(0")
-        lexicon_directory = "../data/bias_related_lexicons"
-        self.lexicons = []
-        self.lexiconNames = []
-        print("LexiconFeatures() init: loading lexicons")
+        #lexicon_directory = "../data/bias_related_lexicons"
+        self.lexicons = lexicons
+        self.lexiconNames = lexiconNames
+        #print("LexiconFeatures() init: loading lexicons")
+        '''
         for filename in os.listdir(lexicon_directory):
             if filename.endswith(".txt"):
                 file = os.path.join(lexicon_directory, filename)
@@ -132,7 +207,7 @@ class LexiconFeatures(BaseEstimator, TransformerMixin):
                 continue
             else:
                 continue
-
+        '''
 
     def fit(self, x, y=None):
         return self
@@ -183,17 +258,19 @@ pipeline = Pipeline([
     ('union', ColumnTransformer(
         [
             # Pulling features from the post's subject line (first column)
-            ('subject', TfidfVectorizer(sublinear_tf=True, max_df=0.5,
-                                 stop_words='english', ngram_range=(1,3)), 0),
+            #('subject', TfidfVectorizer(sublinear_tf=True, max_df=0.5,
+            #                     stop_words='english', ngram_range=(1,4)), 0),
 
             # Pipeline for standard bag-of-words model for body (second column)
             ('body_bow', TfidfVectorizer(sublinear_tf=True, max_df=0.5,
-                                 stop_words='english', ngram_range=(1,3)), 1),
+                                 stop_words='english', ngram_range=(1,4)), 1),
 
-
+            #('pos_features', PosTagFeatures(tokenizer=nltk.word_tokenize), 0 ),
             #('body_bow', Pipeline([
-            #    ('tfidf', TfidfVectorizer()),
-            #    ('best', TruncatedSVD(n_components=10)),
+            #    ('tfidf', TfidfVectorizer(sublinear_tf=True, max_df=0.5, min_df= 5,
+            #                     stop_words='english', ngram_range=(1,3))
+            #     ),
+            #    ('best', TruncatedSVD(n_components=10))
             #]), 1),
 
             # Pipeline for pulling ad hoc features from post's body
@@ -208,7 +285,7 @@ pipeline = Pipeline([
             ('liwc_features', Pipeline([
                 ('stats', LiwcFeatures()),  # returns a list of dicts
                 ('vect', DictVectorizer()),  # list of dicts -> feature matrix
-            ]), 1),
+            ]), 1)
 
 
 
@@ -216,11 +293,12 @@ pipeline = Pipeline([
 
         # weight components in ColumnTransformer
         transformer_weights={
-            'subject': 0.0,
-            'body_bow': 0.8,
-            'surface_features': 0.0,
+            #'subject': 0.5,
+            'body_bow': 0.7,
+            'surface_features': 0.1,
             'lexicon_features': 0.1,
             'liwc_features':0.1
+            #'pos_features':0.1
         }
     )),
 
@@ -229,16 +307,13 @@ pipeline = Pipeline([
     #                                   tol=1e-3))),
 
     # Use a SVC classifier on the combined features
-    ('svc', LinearSVC(penalty="l2", dual=False,
-                                       tol=1e-3)),
+    ('svc', LinearSVC(penalty= "l2", dual=False, tol=1e-3)),
 
     # Use a RandomForest classifier
     #('classification', RandomForestClassifier())
 ])
 
 
-LOAD_DATA_FROM_DISK = True
-CLASSES = 2
 
 if LOAD_DATA_FROM_DISK:
 	texts_train = np.load("../dump/trainRaw")
@@ -274,12 +349,43 @@ else:
 	print("Data dumped to disk!")
 
 print("Size of train, validataion and test sets: " + str(len(labels_train)) + " , " +  str(len(labels_valid))+ " , "  +  str(len(labels_test)))
-print(texts_train[0:3][0:10])
-print(labels_train[0:3])
+texts_train_valid = pd.concat(
+    [pd.Series(texts_train), pd.Series(texts_valid)])
+labels_train_valid = pd.concat(
+    [pd.Series(labels_train), pd.Series(labels_valid)])
+
+print(texts_train_valid[0:3][0:10])
+print(labels_train_valid[0:3])
 
 #Train:
-print("Fitting the model...")
-pipeline.fit(texts_train, labels_train)
+
+parameters = {
+    'union__body_bow__ngram_range':  ((1, 2),(1,3)),
+    'union__body_bow__min_df': (1, 3, 5),
+    #'union__transformer_weights': (dict('lwic_features'=.1, 'lexicon_features'=.1, 'surface_features'=.1, 'body_bow'=.7 )),
+    'svc__penalty':("l2", "l1"),
+    'svc__tol': (1e-2, 1e-3)
+}
+grid_search = GridSearchCV(pipeline, parameters, verbose=True)
+
+#print("Fitting the model...")
+#pipeline.fit(texts_train, labels_train)
+
+print("Performing grid search...")
+print("pipeline:", [name for name, _ in pipeline.steps])
+print("parameters:")
+print(parameters)
+grid_search.fit(texts_train_valid, labels_train_valid)
+#train = grid_search.transform(texts_train)
+#print(train.shape)
+
+print("Best score: %0.3f" % grid_search.best_score_)
+print("Best parameters set:")
+best_parameters = grid_search.best_estimator_.get_params()
+for param_name in sorted(parameters.keys()):
+    print("\t%s: %r" % (param_name, best_parameters[param_name]))
+
+joblib.dump(grid_search.best_estimator_, '../dump/bestSVClassifier.pkl', compress = 1)
 
 
 
@@ -287,15 +393,15 @@ pipeline.fit(texts_train, labels_train)
 
 #Tests:
 print("Results on training data:")
-y = pipeline.predict(texts_train)
+y = grid_search.predict(texts_train)
 print(classification_report(labels_train,   y))
 
 print("Results on validation data:")
-y = pipeline.predict(texts_valid)
+y = grid_search.predict(texts_valid)
 print(classification_report( labels_valid, y))
 
 print("Results on test data:")
-y = pipeline.predict(texts_test)
+y = grid_search.predict(texts_test)
 print(classification_report( labels_test,  y))
 
 
@@ -307,7 +413,7 @@ texts_buzzfeed, labels_buzzfeed = DataLoading.load_data_buzzfeed("../data/buzzfe
 
 print("Test results on data sampled only from snopes (snopes312 dataset manually checked right items -- unseen claims):")
 texts_test, labels_test, texts, labels = DataLoading.balance_data(texts_snopesChecked, labels_snopesChecked , 40, [2,5])
-y = pipeline.predict(texts_test)
+y = grid_search.predict(texts_test)
 print(classification_report(labels_test, y))
 print("confusion matrix:")
 print(confusion_matrix(labels_test, y))
@@ -315,12 +421,12 @@ print(pd.DataFrame({'Predicted': y, 'Expected': labels_test}))
 
 print("Test results on data sampled from emergent dataset (a broad distribution acc. to topic modeling -- possibly some overlapping claims):")
 texts_test, labels_test, texts, labels = DataLoading.balance_data(texts_emergent, labels_emergent , 300, [2,5])
-y = pipeline.predict(texts_test)
+y = grid_search.predict(texts_test)
 print(classification_report(labels_test,y))
 
 print("Test results on data sampled from buzzfeed dataset (a narrow distribution : US election topic -- possibly some overlapping claims):")
 texts_test, labels_test, texts, labels = DataLoading.balance_data(texts_buzzfeed, labels_buzzfeed , 70, [2,5])
-y = pipeline.predict(texts_test)
+y = grid_search.predict(texts_test)
 print(classification_report(labels_test, y))
 
 
